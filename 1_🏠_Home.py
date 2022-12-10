4f34946168c7fc70bdba1cc7789cb3159b5052b7
Walker Azam <wlkr10@uw.edu>
2022-12-09 21:15:47 -0800

    Making a style fix for fucntion decorator

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -75,7 +75,7 @@ if uploaded_file is not None:
                                 "Left Leg COP Displacement(cm) [Med.-Lat.]",
                                 "Right Leg COP Displacement(cm) [Med.-Lat.]"]])
     # Giving an option to download metrics
-    @st.cache
+    @st.cache  # noqa: E301
     def convert_df(df):
         # IMPORTANT: Cache the conversion to prevent computation on every rerun
         return df.to_csv().encode('utf-8')
